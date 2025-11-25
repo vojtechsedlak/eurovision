e6233aa80650572f9ffefe7b7f6343f163c62642
Vojtech Sedlak <vojtech@linden.local>
2025-11-25 09:12:30 -0800

    update github pages

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -231,4 +231,13 @@
 			custom_map: {'dimension1': 'page_type'}
 		});
 	</script>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17759216290"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'AW-17759216290');
+	</script>
 </svelte:head>
